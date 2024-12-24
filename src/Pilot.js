4abc12b53ccc7e3ef240484646c83e9e67128c4d
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-12-24 13:52:56 -0600

    improving the models of the data base

--- src/Pilot.js
+++ src/Pilot.js
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react';
+
+import { db } from './firebase/firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 function Pilot() {
-  console.log("API Key:", process.env.REACT_APP_FIREBASE_API_KEY);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'projects'));
+        const tasksArray = querySnapshot.docs.map(doc => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setTasks(tasksArray);
+      } catch (error) {
+        console.error('Error fetching documents:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(tasks);
+
   return (
     <div>
-
-
       <h1>Pilot</h1>
 
+      <ul>{tasks.map(task => <li key={task.id}>{task.identification}</li>)}</ul>
+    
     </div>
   );
 }
---
+++ src/models/modelOne.js
@@ -0,0 +1,36 @@
+const data = {
+    description: "Instalación de tierras físicas",
+    id: "nBXFfVfyU8kKgiCzwMgf",
+    identification: "24-232",
+    post_inform: {
+      media: {
+        digital_floor_plan: "https://tse1.mm.bing.net/th?id=OIP.5lz0jjfT6F3Y31BIIHCDZQHaEC&pid=Api"
+      },
+      images: [
+        {
+          comments: "no comments",
+          description: "oil trasfo",
+          image: "https://tse2.mm.bing.net/th?id=OIP.gNqrZXomvckfrO2Vbykh2wHaFJ&pid=Api",
+          location: "sub1",
+          data_loader: "Jesus Vergara"
+        }
+      ]
+    },
+    pre_inform: {
+      media: {
+        freehand_floor_plan: "https://thumb.cadbull.com/img/product_img/original/Layout-plan-electric-substation-layout-file-Wed-Jun-2018-05-44-34.png"
+      },
+      images: [
+        {
+          comments: "no comments",
+          description: "oil electrict transformer",
+          image: "https://tse2.mm.bing.net/th?id=OIP.x5L65UjCR2AaEFKoLiAqhAHaG8&pid=Api",
+          location: "sub1",
+          data_loader: "Jesus Vergara"
+        }
+      ]
+    }
+  };
+
+  export default data;
+  
